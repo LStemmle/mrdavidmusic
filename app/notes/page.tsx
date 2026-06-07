de170b57a81bc52c1d40b89dd54ce0ba6d340790
Laura Stemmle <lstemmle@gmail.com>
2026-06-07 13:36:14 -0400

    Add Meta Pixel to /notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Script from "next/script";
 import type { Metadata } from "next";
 import { LaunchBanner, LaunchCtaSubtext } from "./LaunchBanner";
 
@@ -10,6 +11,14 @@ export const metadata: Metadata = {
 export default function Notes() {
   return (
     <>
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">{`
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1360294676019147');
+        fbq('track', 'PageView');
+      `}</Script>
+      <noscript><img height="1" width="1" style={{display:"none"}} src="https://www.facebook.com/tr?id=1360294676019147&ev=PageView&noscript=1" alt="" /></noscript>
+
       <style>{`
         @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400;1,700&family=Lora:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Caveat:wght@600&family=Abril+Fatface&display=swap');
 
